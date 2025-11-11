bb97c1b4f9f354b3f5e1e65e9454dc5ba1250846
Viridiana_Cruz <viridianacruz2010@gmail.com>
2025-11-11 23:36:13 +0100

    Add TOSKO

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -19,7 +19,7 @@ export function CartProvider({ children }) {
     PTM10: { type: "percentage", value: 10, appliesTo: "ALL" },
     CARLOS5: { type: "percentage", value: 5, appliesTo: "ALL" },
     // PROMOLV10: { type: "percentage", value: 10, appliesTo: "Louis Vuitton" },
-    // TOSKO10: { type: "percentage", value: 10, appliesTo: "Toskovat" },
+    TOSKO10: { type: "percentage", value: 10, appliesTo: "Toskovat" },
     FILIPPO10: {
       type: "percentage",
       value: 10,
