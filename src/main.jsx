f38452f43973bab528b6bf64b1ea64d8574611bf
diegoarsuaga2704-design <diego.arsuaga2704@gmail.com>
2026-04-22 10:26:22 -0600

    Agregar Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 import { OrderProvider } from "./context/OrderContext.jsx";
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
     <OrderProvider>
       <CartProvider>
         <App />
+        <Analytics />
       </CartProvider>
     </OrderProvider>
   </StrictMode>
